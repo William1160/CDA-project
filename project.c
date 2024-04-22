0c7942a7f227f92797989450f4d48dff2d813ce3
William1160 <160280842+William1160@users.noreply.github.com>
2024-04-21 20:13:27 -0400

    Add files via upload

--- project.c
+++ project.c
@@ -1,6 +1,5 @@
 #include "spimcore.h"
 
-
 /* ALU */
 /* 10 Points */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
@@ -20,7 +19,7 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
         return 1;
     }
     unsigned index = PC >> 2;
-    if (PC > 16383)
+    if (index > 16383)
     {
         return 1;
     }
@@ -31,129 +30,127 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 
 /* instruction partition */
 /* 10 Points */
-void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
+void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1, unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
-    
-    *op =     (instruction & 0xfc000000) >> 26;        // instructions [31-26]        1111 1100 0000 0000 0000 0000 0000 0000
-    *r1 =     (instruction & 0x03e00000) >> 21;        // instructions [25-21]        0000 0011 1110 0000 0000 0000 0000 0000
-    *r2 =     (instruction & 0x001f0000) >> 16;        // instructions [20-16]        0000 0000 0001 1111 0000 0000 0000 0000
-    *r3 =     (instruction & 0x0000f800) >> 11;        // instructions [15-11]        0000 0000 0000 0000 1111 1000 0000 0000
-    *funct =  (instruction & 0x0000003f);              // instructions [5-0]          0000 0000 0000 0000 0000 0000 0011 1111
-    *offset = (instruction & 0x0000ffff);              // instructions [15-0]         0000 0000 0000 0000 1111 1111 1111 1111
-    *jsec =   (instruction & 0x003fffff);              // instructions [25-0]         0000 0011 1111 1111 1111 1111 1111 1111
 
+    *op = (instruction & 0xfc000000) >> 26; // instructions [31-26]        1111 1100 0000 0000 0000 0000 0000 0000
+    *r1 = (instruction & 0x03e00000) >> 21; // instructions [25-21]        0000 0011 1110 0000 0000 0000 0000 0000
+    *r2 = (instruction & 0x001f0000) >> 16; // instructions [20-16]        0000 0000 0001 1111 0000 0000 0000 0000
+    *r3 = (instruction & 0x0000f800) >> 11; // instructions [15-11]        0000 0000 0000 0000 1111 1000 0000 0000
+    *funct = (instruction & 0x0000003f);    // instructions [5-0]          0000 0000 0000 0000 0000 0000 0011 1111
+    *offset = (instruction & 0x0000ffff);   // instructions [15-0]         0000 0000 0000 0000 1111 1111 1111 1111
+    *jsec = (instruction & 0x003fffff);     // instructions [25-0]         0000 0011 1111 1111 1111 1111 1111 1111
 }
-
-
 
 /* instruction decode */
 /* 15 Points */
-int instruction_decode(unsigned op,struct_controls *controls)
+int instruction_decode(unsigned op, struct_controls *controls)
 {
-    switch(op) {
-        case 0x00:                    // R-type
-            controls->RegDst = '1';
-            controls->Jump = '0';
-            controls->Branch = '0';
-            controls->Memread = '0';
-            controls->MemtoReg = '0';
-            controls->ALUop = '7';
-            controls->MemWrite = '0';
-            controls->ALUSrc = '0';
-            controls->RegWrite = '1';
-            break;
-        case 0x02:                    // jump
-            controls->RegDst = '0';
-            controls->Jump = '1';
-            controls->Branch = '0';
-            controls->Memread = '0';
-            controls->MemtoReg = '0';
-            controls->ALUop = '0';
-            controls->MemWrite = '0';
-            controls->ALUSrc = '0';
-            controls->RegWrite = '1';
-            break;
-        case 0x04:                    // beq
-            controls->RegDst = '2';
-            controls->Jump = '0';
-            controls->Branch = '1';
-            controls->Memread = '0';
-            controls->MemtoReg = '2';
-            controls->ALUop = '1';
-            controls->MemWrite = '0';
-            controls->ALUSrc = '2';
-            controls->RegWrite = '0';
-            break;
-        case 0x08:                    // addi
-            controls->RegDst = '0';
-            controls->Jump = '0';
-            controls->Branch = '0';
-            controls->Memread = '0';
-            controls->MemtoReg = '0';
-            controls->ALUop = '0';
-            controls->MemWrite = '0';
-            controls->ALUSrc = '1';
-            controls->RegWrite = '1';
-            break;
-        case 0x0a:                    // slti
-            controls->RegDst = '0';
-            controls->Jump = '0';
-            controls->Branch = '0';
-            controls->Memread = '0';
-            controls->MemtoReg = '0';
-            controls->ALUop = '2';
-            controls->MemWrite = '0';
-            controls->ALUSrc = '1';
-            controls->RegWrite = '1';
-            break;
-        case 0x0b:                    // sltiu
-            controls->RegDst = '0';
-            controls->Jump = '0';
-            controls->Branch = '0';
-            controls->Memread = '0';
-            controls->MemtoReg = '0';
-            controls->ALUop = '3';
-            controls->MemWrite = '0';
-            controls->ALUSrc = '1';
-            controls->RegWrite = '1';
-            break;
-        case 0x0f:                    // lui
-            controls->RegDst = '0';
-            controls->Jump = '0';
-            controls->Branch = '0';
-            controls->Memread = '0';
-            controls->MemtoReg = '0';
-            controls->ALUop = '6';
-            controls->MemWrite = '0';
-            controls->ALUSrc = '1';
-            controls->RegWrite = '1';
-            break;
-        case 0x23:                    // lw
-            controls->RegDst = '0';
-            controls->Jump = '0';
-            controls->Branch = '0';
-            controls->Memread = '1';
-            controls->MemtoReg = '1';
-            controls->ALUop = '0';
-            controls->MemWrite = '0';
-            controls->ALUSrc = '1';
-            controls->RegWrite = '1';
-            break;
-        case 0x2b:                    // sw
-            controls->RegDst = '2';
-            controls->Jump = '0';
-            controls->Branch = '0';
-            controls->Memread = '0';
-            controls->MemtoReg = '2';
-            controls->ALUop = '0';
-            controls->MemWrite = '1';
-            controls->ALUSrc = '1';
-            controls->RegWrite = '0';
-            break;
-        default:
-            return 1;
+    switch (op)
+    {
+    case 0x00: // R-type
+        controls->RegDst = '1';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '0';
+        controls->MemtoReg = '0';
+        controls->ALUOp = '7';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '0';
+        controls->RegWrite = '1';
+        break;
+    case 0x02: // jump
+        controls->RegDst = '0';
+        controls->Jump = '1';
+        controls->Branch = '0';
+        controls->MemRead = '0';
+        controls->MemtoReg = '0';
+        controls->ALUOp = '0';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '0';
+        controls->RegWrite = '1';
+        break;
+    case 0x04: // beq
+        controls->RegDst = '2';
+        controls->Jump = '0';
+        controls->Branch = '1';
+        controls->MemRead = '0';
+        controls->MemtoReg = '2';
+        controls->ALUOp = '1';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '2';
+        controls->RegWrite = '0';
+        break;
+    case 0x08: // addi
+        controls->RegDst = '0';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '0';
+        controls->MemtoReg = '0';
+        controls->ALUOp = '0';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '1';
+        controls->RegWrite = '1';
+        break;
+    case 0x0a: // slti
+        controls->RegDst = '0';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '0';
+        controls->MemtoReg = '0';
+        controls->ALUOp = '2';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '1';
+        controls->RegWrite = '1';
+        break;
+    case 0x0b: // sltiu
+        controls->RegDst = '0';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '0';
+        controls->MemtoReg = '0';
+        controls->ALUOp = '3';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '1';
+        controls->RegWrite = '1';
+        break;
+    case 0x0f: // lui
+        controls->RegDst = '0';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '0';
+        controls->MemtoReg = '0';
+        controls->ALUOp = '6';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '1';
+        controls->RegWrite = '1';
+        break;
+    case 0x23: // lw
+        controls->RegDst = '0';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '1';
+        controls->MemtoReg = '1';
+        controls->ALUOp = '0';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '1';
+        controls->RegWrite = '1';
+        break;
+    case 0x2b: // sw
+        controls->RegDst = '2';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '0';
+        controls->MemtoReg = '2';
+        controls->ALUOp = '0';
+        controls->MemWrite = '1';
+        controls->ALUSrc = '1';
+        controls->RegWrite = '0';
+        break;
+    default:
+        return 1;
     }
-    
+
     return 0;
 }
 
@@ -170,7 +167,13 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-
+    if((offset & 0x1) == 1){
+        *extended_value = offset << 16;
+        *extended_value = *extended_value + 0xFFFF;
+    }
+    else{
+        *extended_value = offset << 16;
+    }
 }
 
 /* ALU operations */
@@ -202,4 +205,7 @@ void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char 
 
 }
 
+void WinMain(){
+
+}
 // ./cisproject/spimcore cisproject/input_file.asc
